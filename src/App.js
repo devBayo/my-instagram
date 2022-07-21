8d1d1f9e3f0b00ab5e1434c8050b79167148a2e2
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-07-21 05:02:41 +0100

    add default gallery images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { Nav, Bio } from './components';
+import { Nav, Bio, Gallery } from './components';
 import './App.css';
 
 const App = () => {
   return (
-    <div className='container'>
+    <>
       <Nav />
-      <Bio />
-    </div>
+      <div className='container'>
+        <Bio />
+        <Gallery />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,41 @@
+import pexels1 from '../assets/pexels1.jpeg';
+import pexels2 from '../assets/pexels2.jpeg';
+import pexels3 from '../assets/pexels3.jpeg';
+import pexels4 from '../assets/pexels4.jpeg';
+import pexels5 from '../assets/pexels5.jpeg';
+
+const Gallery = () => {
+  return (
+    <>
+      <input type='file' name='photo' id='addPhotoInput' />
+      <label htmlFor='addPhotoInput'>
+        <i className='add-photo-button fas fa-plus-square'></i>
+      </label>
+
+      <section className='gallery'>
+        <div className='item'>
+          <img src={pexels1} className='item-image' alt='' />
+          <button className='delete-button'>Delete</button>
+        </div>
+        <div className='item'>
+          <img src={pexels2} className='item-image' alt='' />
+          <button className='delete-button'>Delete</button>
+        </div>
+        <div className='item'>
+          <img src={pexels3} className='item-image' alt='' />
+          <button className='delete-button'>Delete</button>
+        </div>
+        <div className='item'>
+          <img src={pexels4} className='item-image' alt='' />
+          <button className='delete-button'>Delete</button>
+        </div>
+        <div className='item'>
+          <img src={pexels5} className='item-image' alt='' />
+          <button className='delete-button'>Delete</button>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Gallery;
