a4238ad6943e5b2f25c796a595167b207a0cf554
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-07-21 05:49:13 +0100

    use state to change Bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,15 +1,31 @@
+import { useState } from 'react';
 import profileIcon from '../assets/profileIcon.svg';
 
 const Bio = () => {
+  const [userDetails, setUserDetails] = useState({
+    name: 'Ebenezer Don',
+    about:
+      ' Building Newdev.io - Learn to code and connect with the best minds',
+  });
+
+
+  const updateUserDetails = (event) => {
+    event.preventDefault();
+    setUserDetails({
+      name: event.target.nameOfUser.value,
+      about: event.target.aboutUser.value,
+    });
+  };
+
   const editForm = (
-    <form className='edit-bio-form'>
-      <input type='text' id='' placeholder='Your name' />
-      <input type='text' id='' placeholder='About you' />
+    <form className='edit-bio-form' onSubmit={(e) => updateUserDetails(e)}>
+      <input type='text' id='' name='nameOfUser' placeholder='Your name' />
+      <input type='text' id='' name='aboutUser' placeholder='About you' />
       <br />
       <button type='button' className='cancel-button'>
         Cancel
       </button>
-      <button type='button'>Save</button>
+      <button type='submit'>Save</button>
     </form>
   );
   return (
@@ -18,10 +34,8 @@ const Bio = () => {
         <img src={profileIcon} alt='profile' />
       </div>
       <div className='profile-info'>
-        <p className='name'>Ebenezer Don</p>
-        <p className='about'>
-          Building Newdev.io - Learn to code and connect with the best minds
-        </p>
+        <p className='name'>{userDetails.name}</p>
+        <p className='about'>{userDetails.about}</p>
         <button>Edit</button>
         {editForm}
       </div>
