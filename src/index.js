007b1df2a298c77524f08a9cbb36c20281e033b8
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-08-03 20:51:37 +0100

    sw.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+serviceWorker.register();
